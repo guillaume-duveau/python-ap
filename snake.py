b9293fefba075fca5606b94b016ba7787ca0297d
Guillaume <guillaume.duveau@etu.minesparis.psl.eu>
2023-11-14 15:06:08 +0100

    ^première implémentation des arguments en ligne de commande

--- snake.py
+++ snake.py
@@ -1,17 +1,47 @@
 # importation des modules 
 import pygame as pg
+import argparse
 
 #CONSTANTES 
-BLACK,SCREEN_WIDTH,SCREEN_HEIGHT,CLOCK_FREQUENCY=(0,0,0),400,300,5.5
+BLACK,WIDTH,HEIGHT,FPS=(0,0,0),400,300,5.5
 WHITE=(255,255,255)
 GREEN=(0,255,0)
 RED=(255,0,0)
-_SIZE=20
+TILE_SIZE=20
 H=(0,-1)
 B=(0,+1)
 D=(1,0)
 G=(-1,0)
 dict_direct={"h":H,"b":B,"d":D,"g":G}
+
+#définiton des arguments de la fonction 
+parser = argparse.ArgumentParser(description='Some description.')
+parser.add_argument('--bg-color-1', default=WHITE,help="first color of the background checkerboard")
+parser.add_argument('--bg-color-2', default=BLACK,help="second color of the background checkerboard")
+parser.add_argument('-height', type=int, default=HEIGHT, help="height of the window")
+parser.add_argument('-width', type=int, default=WIDTH, help="height of the window")
+parser.add_argument('--fps', type=int, help="number of frames per second")
+parser.add_argument('--fruit-color', help="color of the fruit")
+parser.add_argument('--snake-color', help="color of the snake")
+parser.add_argument('--snake-length', help="length of the snake")
+parser.add_argument('--tile-size', help="size of a tail size")
+args = parser.parse_args()
+print(args)
+
+# vérification des arguments
+if (args.bg_color_1 ==  args.bg_color_2) or (args.bg_color_1 ==  args.snake_color) or (args.bg_color_2 ==  args.snake_color):
+    raise ValueError("Les couleurs doivent être diférentes") # on vérifie que les couleurs sont différentes
+
+if (args.height % args.tile_size) or (args.width % args.tile_size) or (args.width // args.tile_size < 20 ) or (args.height // args.tile_size < 12 ): 
+    raise ValueError("La taille est insuffisante")
+ 
+if args.snake_length < 2:
+    raise ValueError("le serpent est trop court")
+
+
+ 
+
+
 
 
 # intialisation de l'affichage du jeu 
@@ -23,7 +53,7 @@ sh=True
 # Caractéristiques du serpent 
 pos=(5,10) # colonne, ligne 
 long_p=3
-long=long_p
+long=long_p # long est la longueur actuelle et long_p la longueur précédente
 direct_p="d"
 serpent=[tuple(map(lambda i,j : i+j , pos ,tuple(dict_direct[direct_p])))]
 for k in range(long):
@@ -47,22 +77,21 @@ while sh:
     direct=direct_p # sans changement de direction, le serpent continue tout droit 
     for event in pg.event.get():
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_UP:
+            if event.key == pg.K_UP: # aller en haut 
                 direct="h"
-            elif event.key == pg.K_DOWN:
+            elif event.key == pg.K_DOWN: # aller en bas 
                 direct="b"
-            elif event.key == pg.K_RIGHT:
+            elif event.key == pg.K_RIGHT: # aller à droite
                 direct="d"
-            elif event.key == pg.K_LEFT:
+            elif event.key == pg.K_LEFT: # aller à gauche 
                 direct="g"    
-            elif event.key == pg.K_q:
+            elif event.key == pg.K_q: # presser la touche q 
                 sh=False
         if event.type == pg.QUIT:
             sh=False
 
 #gestion de l'affichage du fruit 
     pg.draw.rect(SCREEN,RED,(fruit[0]*_SIZE,fruit[1]*_SIZE,_SIZE,_SIZE))
-
 
 #gestion de la position du serpent 
 
@@ -90,7 +119,6 @@ while sh:
     pg.display.update()
 
 # gestion de l'affichage du score 
-
-    pg.set_caption(f"snake-score:{long-3}")
+    """pg.set_caption(f"snake-score:{long-3}")""" 
 pg.quit()
 quit(0)
