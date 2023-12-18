135c743243984f5395df7aa675e0acd2f2703d69
Guillaume <guillaume.duveau@etu.minesparis.psl.eu>
2023-12-18 21:28:38 +0100

    les tests

--- tests/test_snake.py
+++ tests/test_snake.py
@@ -1,9 +1,9 @@
-serpent=[(4,0),(3,0),(2,0),(1,0)]
-sh,direct,width,height,tile_size,posp,serpent,fruit,v_gameover_on_exit,v_g=True,"d",20,12,20,(4,0),serpent,(0,0),False,True
+serpent=[(4,0),(3,0),(2,0),(1,0)] # liste initiale des positions
+sh,direct,width,height,tile_size,posp,serpent,fruit,v_gameover_on_exit,v_g=True,"d",20,12,20,(4,0),serpent,(0,0),False,True # paramètres initiaux du jeu 
 
 
 def test_get_score():
     assert snake.get_score(serpent)==0
 
 def test_move_snake():
-    assert move_snake(sh,direct,width,height,tile_size,posp,serpent,fruit,v_gameover_on_exit,v_g)==[(5,0),(4,0),(3,0),(2,0)],(1,0),True
+    assert move_snake(sh,direct,width,height,tile_size,posp,serpent,fruit,v_gameover_on_exit,v_g)==[(5,0),(4,0),(3,0),(2,0)],(5,0),True # On vérifie si la position du serpent passe bien de (1,0) à 
